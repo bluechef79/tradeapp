3098c6350c737bd88c781e36008dd1e98bc3816c
Caleb Horner <chorner79@gmail.com>
2017-09-21 19:01:59 -0400

    added ajax, friends etc

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
   get 'my_friends' => 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+  resources :users, only: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
